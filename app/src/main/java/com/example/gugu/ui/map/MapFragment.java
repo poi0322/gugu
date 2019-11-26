425f9c8d88776f3d49c5a3978fd4e28852782130
POI <poi0322@naver.com>
2019-11-26 16:24:18 +0900

    map쪽 어뎁티브뷰 완성

--- app/src/main/java/com/example/gugu/ui/map/MapFragment.java
+++ app/src/main/java/com/example/gugu/ui/map/MapFragment.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.gugu.R;
 
+import java.util.List;
+
 public class MapFragment extends Fragment {
 
     private MapViewModel mapViewModel;
+    private ListView mapListView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,14 +31,24 @@ public class MapFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
         //final TextView textView = root.findViewById(R.id.text_map);
 
-
-
         mapViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s);
             }
         });
+
+        mapListView = root.findViewById(R.id.map_list);
+
+        listSetting();
+
+
         return root;
+    }
+    private void listSetting(){
+        ListItemAdapter listItemAdapter = new ListItemAdapter();
+
+        listItemAdapter.addItem(getResources().getDrawable(R.drawable.ic_alarm),"ddd","sadf",getResources().getDrawable(R.drawable.ic_alarm),getResources().getDrawable(R.drawable.ic_alarm),getResources().getDrawable(R.drawable.ic_alarm),getResources().getDrawable(R.drawable.ic_alarm));
+        mapListView.setAdapter(listItemAdapter);
     }
 }
---
+++ app/src/main/java/com/example/gugu/ui/map/MapListItem.java
@@ -0,0 +1,69 @@
+package com.example.gugu.ui.map;
+
+import android.graphics.drawable.Drawable;
+
+public class MapListItem {
+    private Drawable listImage;
+    private String listTitle;
+    private String listProfile;
+    private Drawable list_active_sup;
+    private Drawable list_bath_sup;
+    private Drawable list_toilet_sup;
+    private Drawable list_clean_sup;
+
+    public Drawable getListImage() {
+        return listImage;
+    }
+
+    public void setListImage(Drawable listImage) {
+        this.listImage = listImage;
+    }
+
+    public String getListTitle() {
+        return listTitle;
+    }
+
+    public void setListTitle(String listTitle) {
+        this.listTitle = listTitle;
+    }
+
+    public String getListProfile() {
+        return listProfile;
+    }
+
+    public void setListProfile(String listProfile) {
+        this.listProfile = listProfile;
+    }
+
+    public Drawable getList_active_sup() {
+        return list_active_sup;
+    }
+
+    public void setList_active_sup(Drawable list_active_sup) {
+        this.list_active_sup = list_active_sup;
+    }
+
+    public Drawable getList_bath_sup() {
+        return list_bath_sup;
+    }
+
+    public void setList_bath_sup(Drawable list_bath_sup) {
+        this.list_bath_sup = list_bath_sup;
+    }
+
+    public Drawable getList_toilet_sup() {
+        return list_toilet_sup;
+    }
+
+    public void setList_toilet_sup(Drawable list_toilet_sup) {
+        this.list_toilet_sup = list_toilet_sup;
+    }
+
+    public Drawable getList_clean_sup() {
+        return list_clean_sup;
+    }
+
+    public void setList_clean_sup(Drawable list_clean_sup) {
+        this.list_clean_sup = list_clean_sup;
+    }
+}
